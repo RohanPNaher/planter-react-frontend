29a71bd5dafa3d7beba3dcaab331d6cdc1750bef
RohanPNaher <RohanPNaher@gmail.com>
2022-04-13 00:41:17 -0400

    modify profile service

--- src/services/profileService.js
+++ src/services/profileService.js
@@ -3,9 +3,7 @@ import * as tokenService from '../services/tokenService'
 const BASE_URL = `${process.env.REACT_APP_API_URL}/api/profiles`
 
 async function getAllProfiles() {
-  const res = await fetch(BASE_URL, {
-    headers: { Authorization: `Bearer ${tokenService.getToken()}` },
-  })
+  const res = await fetch(BASE_URL)
   return await res.json()
 }
 
