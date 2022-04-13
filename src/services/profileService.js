5d82008858fb1f4b5c0a967fc98086a81a78f547
RohanPNaher <RohanPNaher@gmail.com>
2022-04-13 01:02:34 -0400

    add getall gardens method for gardens service

--- src/services/profileService.js
+++ src/services/profileService.js
@@ -3,7 +3,17 @@ import * as tokenService from '../services/tokenService'
 const BASE_URL = `${process.env.REACT_APP_API_URL}/api/profiles`
 
 async function getAllProfiles() {
-  const res = await fetch(BASE_URL)
+  const res = await fetch(BASE_URL, {
+    // this gives a CORS disallow (redirect, preflight?, Status code: 308)
+    // headers: { Authorization: `Bearer ${tokenService.getToken()}` },
+  })
+  return await res.json()
+}
+
+async function getSingleProfile(id) {
+  const res = await fetch(`${BASE_URL}/api/profiles/${id}`, {
+    // headers: { Authorization: `Bearer ${tokenService.getToken()}` },
+  })
   return await res.json()
 }
 
