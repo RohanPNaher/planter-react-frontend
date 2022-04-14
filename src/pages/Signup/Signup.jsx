b5767d112706e766732e4e08ed47d58c50223ec2
RohanPNaher <RohanPNaher@gmail.com>
2022-04-14 17:14:29 -0400

    adjust signup and login css

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -11,9 +11,13 @@ const Signup = props => {
 
   return (
     <main className={styles.container}>
-      <h1>Sign Up</h1>
-      <p>{message}</p>
-      <SignupForm {...props} updateMessage={updateMessage} />
+      <div className={styles.brownBorder}>
+        <div className={styles.greenBorder}>
+          <h1>Sign Up</h1>
+          <p>{message}</p>
+          <SignupForm {...props} updateMessage={updateMessage} />
+        </div>
+      </div>
     </main>
   )
 }
